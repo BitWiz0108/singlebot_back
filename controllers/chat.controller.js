de31080c1392e03a8d32829984a95eb4d76b612e
Marco-Ming <marco@lionsandlegacy.com>
2023-04-06 11:26:38 -0700

    1

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,6 +1,4 @@
 import { Configuration, OpenAIApi } from "openai";
-import fs from "fs";
-import Axios from "axios";
 
 const openAIConfig = new Configuration({
   apiKey: process.env.OPENAI_KEY
@@ -8,16 +6,16 @@ const openAIConfig = new Configuration({
 const openapi = new OpenAIApi(openAIConfig);
 
 export const chatCompletion = async (req, res) => {
-
+  const base_prompt = `I would say "hey," and then you would say "Hey there!". Don't try to finish our conversation. Let us go through just like good friends conversation go. `;
   try {
     const { prompt } = req.body;
     const answer = await openapi.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
+      messages: [{ role: "user", content: base_prompt + prompt }],
       temperature: 0,
       max_tokens: 3000
     });
-
+    console.log("This is the real prompt that the back-end is saying:", base_prompt + prompt);
     const text = answer.data.choices[0].message;
     res.status(200).json({ text });
   } catch (err) {
@@ -26,9 +24,3 @@ export const chatCompletion = async (req, res) => {
     });
   }
 };
-
-
-
-
-
-
