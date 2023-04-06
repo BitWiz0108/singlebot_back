78764727ba46e0c1a261b2149c0901eae75f13c1
Marco-Ming <marco@lionsandlegacy.com>
2023-04-06 16:43:48 -0700

    1

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -6,21 +6,31 @@ const openAIConfig = new Configuration({
 const openapi = new OpenAIApi(openAIConfig);
 
 export const chatCompletion = async (req, res) => {
-  const base_prompt = `I would say "hey," and then you would say "Hey there!". Don't try to finish our conversation. Let us go through just like good friends conversation go. `;
-  try {
-    const { prompt } = req.body;
-    const answer = await openapi.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: base_prompt + prompt }],
-      temperature: 0,
-      max_tokens: 3000
-    });
-    console.log("This is the real prompt that the back-end is saying:", base_prompt + prompt);
-    const text = answer.data.choices[0].message;
-    res.status(200).json({ text });
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    });
+  const { prompt } = req.body;
+  console.log("!!@!@!@!@", prompt);
+
+  if (prompt == " Hey") {
+    res.status(200).json({ text: { role: "assistant", content: "Hey there!" } })
+  } else if (prompt == " hey") {
+    res.status(200).json({ text: { role: "assistant", content: "Hey there!" } })
+  } else {
+    const base_prompt = `Imagine you are a chatbot to help me. NOT the AI language model. Do not repeat the same responses.
+    You can say me "Hey there", when only I say "Hi!".
+    Don't try to finish our conversation. Let us go through just like good friends conversation go. Let us make our conversation natural and friendly. `;
+    try {
+      const answer = await openapi.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: base_prompt + prompt }],
+        temperature: 0,
+        max_tokens: 3000
+      });
+      console.log("This is the real prompt that the back-end is saying:", base_prompt + prompt);
+      const text = answer.data.choices[0].message;
+      res.status(200).json({ text });
+    } catch (err) {
+      res.status(500).json({
+        message: err.message
+      });
+    }
   }
 };
