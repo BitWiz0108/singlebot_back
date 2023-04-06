21e3c160d8eac5fa362cdbc29e99dfabd82d7888
Marco-Ming <marco@lionsandlegacy.com>
2023-04-06 06:44:10 -0700

    changes

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -6,38 +6,19 @@ const openAIConfig = new Configuration({
   apiKey: process.env.OPENAI_KEY
 });
 const openapi = new OpenAIApi(openAIConfig);
-const training_data = fs.createReadStream("training_data.json");
-
-// export const fineTune = async (req, res) => {
-//   try {
-//     const response1 = await openapi.createFile(
-//       training_data,
-//       "fine-tune"
-//     );
-//     const response2 = await openapi.createFineTune({
-//       training_file: 'file-W24oryeTLfsQCtgsmdR5xbWs',
-//     });
-//     res.status(200).json({
-//       response2
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       message: err.message
-//     })
-//   }
-// }
 
 export const chatCompletion = async (req, res) => {
+
   try {
     const { prompt } = req.body;
-    const answer = await openapi.createCompletion({
-      // model: "davinci:ft-vine-strategies-2023-04-05-06-24-27",
-      model: "text-davinci-003",
-      prompt: prompt,
+    const answer = await openapi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
       temperature: 0,
       max_tokens: 3000
     });
-    const text = answer.data.choices[0].text;
+
+    const text = answer.data.choices[0].message;
     res.status(200).json({ text });
   } catch (err) {
     res.status(500).json({
