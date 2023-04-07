1c44de255ac74e7e9f9a0aedc08315c5e3180d46
Marco-Ming <marco@lionsandlegacy.com>
2023-04-07 07:09:29 -0700

    123

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -13,21 +13,31 @@ export const chatCompletion = async (req, res) => {
     console.log("Here!!!");
     const answer = await openapi.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
+      messages: [{ role: "user", content: prompt.toLowerCase() }],
       temperature: 0.5,
       max_tokens: 1000
     });
-    // setTimeout(() => {
     res.status(200).json({ text: { role: "assistant", content: "Hey there, How are you?" } })
-    // }, 1500);
+  } else if (prompt == "Ok" || prompt == "Okay" || prompt == "ok" || prompt == "okay") {
+    const answer = await openapi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt.toLowerCase() }],
+      temperature: 0.5,
+      max_tokens: 1000
+    });
+    res.status(200).json({ text: { role: "assistant", content: "Great! So what is next?" } })
   } else {
-    const base_prompt = `Imagine you are a chatbot to help me. NOT the AI language model. Do not repeat the same responses.
-    Don't try to finish our conversation. Let us go through just like good friends conversation go. Let us make our conversation natural and friendly. `;
+    const base_prompt = `Imagine you are a man and a good friend of mine. NOT the AI language model. Do not repeat the same responses. Do not try to complete or finish my sentence.
+    Let us communicate just like how good friends conversation go. Let us make our conversation natural and friendly. `;
     try {
       const answer = await openapi.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: base_prompt + prompt }],
+        messages: [{ role: "user", content: prompt.toLowerCase() + " ." }],
         temperature: 0.5,
+        // stream: true,
+        presence_penalty: 1,
+        frequency_penalty: 1,
+        // logit_bias: true,
         max_tokens: 1000
       });
       // console.log("This is the real prompt that the back-end is saying:", base_prompt + prompt);
