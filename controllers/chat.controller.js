ff291f3ce0f32f4183b5507511dc8bb2bcee63ed
Marco-Ming <marco@lionsandlegacy.com>
2023-04-07 06:24:02 -0700

    12

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -7,12 +7,19 @@ const openapi = new OpenAIApi(openAIConfig);
 
 export const chatCompletion = async (req, res) => {
   const { prompt } = req.body;
-  console.log("!!@!@!@!@", prompt);
+  // console.log("!!@!@!@!@", prompt);
 
-  if (prompt == "Hey" || "hey" || "hi" || "Hi" || "Hi," || "Hey there," || "Hey there" || "Hi!" || "Hi there," || "Hi there") {
-    setTimeout(() => {
-      res.status(200).json({ text: { role: "assistant", content: "Hey, How are you?" } })
-    }, 1500);
+  if (prompt == "Hey" || prompt == "hey" || prompt == "hi" || prompt == "Hi" || prompt == "Hi," || prompt == "Hey there," || prompt == "Hey there" || prompt == "Hi!" || prompt == "Hi there," || prompt == "Hi there") {
+    console.log("Here!!!");
+    const answer = await openapi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.5,
+      max_tokens: 1000
+    });
+    // setTimeout(() => {
+    res.status(200).json({ text: { role: "assistant", content: "Hey, How are you?" } })
+    // }, 1500);
   } else {
     const base_prompt = `Imagine you are a chatbot to help me. NOT the AI language model. Do not repeat the same responses.
     Don't try to finish our conversation. Let us go through just like good friends conversation go. Let us make our conversation natural and friendly. `;
@@ -20,10 +27,10 @@ export const chatCompletion = async (req, res) => {
       const answer = await openapi.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: base_prompt + prompt }],
-        temperature: 0,
-        max_tokens: 3000
+        temperature: 0.5,
+        max_tokens: 1000
       });
-      console.log("This is the real prompt that the back-end is saying:", base_prompt + prompt);
+      // console.log("This is the real prompt that the back-end is saying:", base_prompt + prompt);
       const text = answer.data.choices[0].message;
       res.status(200).json({ text });
     } catch (err) {
